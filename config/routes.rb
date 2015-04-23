ace44f72c7209fe9bbfec5b02d0d434f5ea5a1b2
mcstarsa <mcstarsa@gmail.com>
2015-04-23 09:45:20 +0800

    vote route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :topics
+  #投票加分操作加一個 route
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
